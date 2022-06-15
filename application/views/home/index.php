2b45e96f88670c0bb0ea1a70bcf9476f6f300a61
Fadhilatul Muthiah <fadhilatulmutia@gmail.com>
2022-06-15 17:34:16 +0700

    first commit

---
+++ application/views/home/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sistem Informasi Wisuda</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/assets-images/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Simple line icons-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand"><a href="#page-top">Sistem Informasi Wisuda</a></li>
+                <li class="sidebar-nav-item"><a href="#page-top">Home</a></li>
+                <li class="sidebar-nav-item"><a href="#informasiwisuda">Informasi Wisuda</a></li>
+                <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <!-- Header-->
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h1 class="mb-1">SISTEM INFORMASI WISUDA</h1>
+                <h3 class="mb-5"><em>Universitas Nusa Mandiri</em></h3>
+                <a class="btn btn-primary btn-xl" href="login/index">Login</a>
+            </div>
+        </header>
+        <!-- Informasi Wisuda-->
+        <section class="content-section bg-light" id="informasiwisuda">
+            <div class="container px-4 px-lg-5 text-center">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <h2>Pelaksanaan Wisuda Ke-99 Universitas Nusa Mandiri</h2>
+                        <br>
+                        <h2>Semester Ganjil Tahun Akademik 2022/2023</h2>
+                        <br>
+                        <br>
+                        <p class="lead mb-5">
+                            Diinformasikan kepada seluruh Wisudawan Semester Ganjil TA.2022/2023 pelaksanaan Wisuda ke-68 Semester Ganjil TA.2021/2022 akan dilaksanakan secara offline/tatap muka. Dalam rangka mencegah penyebaran COVID-19 pelaksanaan Wisuda diatur sebagai berikut:
+                        </p>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <a class="btn btn-dark btn-xl" href="#link google drive mungkin">Download Informasi Pelaksanaan Wisuda</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact-->
+        <div class="map" id="contact">
+            <div class="container px-4 px-lg-5 text-center">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <br>
+                        <br>
+                        <h2>Universitas Nusa Mandiri Margonda</h2>
+                        <p class="lead mb-5">
+                            Jl. Margonda Raya No.545, RW.7, Pondok Cina, Kecamatan Beji, Kota Depok, Jawa Barat 16424
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15861.118873937758!2d106.8317871!3d-6.3578286!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x73a3d74cfe631348!2sUniversitas%20Nusa%20Mandiri%20Kampus%20Margonda!5e0!3m2!1sen!2sid!4v1652350484472!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            
+            <br />
+        </div>
+        <br>
+        <br>
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container px-4 px-lg-5">
+                <ul class="list-inline mb-5">
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-facebook"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-twitter"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white" href="#!"><i class="icon-social-github"></i></a>
+                    </li>
+                </ul>
+                <p class="text-muted small mb-0">Tugas Besar Matakuliah Advanced Software Engineering</p>
+            </div>
+        </footer>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+    </body>
+</html>
---
+++ application/views/login/login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>LOGIN | Sistem Informasi Wisuda</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="assets/images-login/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/fonts-login/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/fonts-login/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="assets/vendor-login/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/css-login/util.css">
+	<link rel="stylesheet" type="text/css" href="assets/css-login/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images-login/bg-graduation.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					LOGIN
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5">
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="NIM">
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<a class="btn btn-primary btn-xl" href="C:\Users\nessa\Downloads\TUBES SE\SISTEM INFORMASI WISUDA\dashboard\dashboard.html">Login</a>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/bootstrap/js/popper.js"></script>
+	<script src="assets/vendor-login/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/daterangepicker/moment.min.js"></script>
+	<script src="assets/vendor-login/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor-login/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
